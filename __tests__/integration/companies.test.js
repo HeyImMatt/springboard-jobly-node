554ec439436a40297f63a36f9a0aee0915a631bb
heyimmatt <mrmcfarland@gmail.com>
2020-10-23 16:15:58 -0700

    Add tests for /companies post route

--- __tests__/integration/companies.test.js
+++ __tests__/integration/companies.test.js
@@ -35,11 +35,33 @@ afterAll(async () => {
   }
 })
 
-describe('GET /companies', async () => {
+describe('GET /companies', () => {
   test('Gets all company data', async () => {
     const resp = await request(app).get('/companies');
     expect(resp.statusCode).toBe(200);
     expect(resp.body.companies).toHaveLength(1);
     expect(resp.body.companies[0]).toHaveProperty('handle');
     })
+})
+
+describe('POST /companies', () => {
+  test('Creates a new company', async () => {
+    const company = {
+      handle: 'newco',
+      name: 'New Company',
+      description: 'This is the new company',
+      num_employees: 10,
+      logo_url: 'https://cdn.pixabay.com/photo/2016/08/25/07/30/orange-1618917_1280.png',
+    }
+    const resp = await request(app).post('/companies').send(company);
+
+    expect(resp.statusCode).toBe(201);
+    expect(resp.body.company).toEqual(company);
+  });
+
+  test('Missing data fails json validation', async () => {
+    const resp = await request(app).post('/companies').send({handle: 'failco'});
+
+    expect(resp.statusCode).toBe(400);
+  })
 })
