e2624436ccfe4ba97bc560f4bac22bc9133e60c6
heyimmatt <mrmcfarland@gmail.com>
2020-10-24 09:58:49 -0700

    Add user json schemas and routes

---
+++ routes/users.js
@@ -0,0 +1,75 @@
+const express = require('express');
+const ExpressError = require('../helpers/expressError');
+const User = require('../models/user')
+const Ajv = require('ajv');
+const userNewSchema = require('../schemas/userNewSchema.json')
+const userUpdateSchema = require('../schemas/userUpdateSchema.json')
+
+const router = new express.Router();
+
+const ajv = new Ajv();
+
+router.get('/', async (req, res, next) => {
+  try {
+    const users = await User.findAll();
+    return res.json({ users });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+router.get('/:username',async (req, res, next) => {
+  try {
+    const user = await User.findOne(req.params.username);
+    return res.json({ user });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+router.post('/', async (req, res, next) => {
+  try {
+    const valid = ajv.validate(userNewSchema, req.body);
+    if (!valid) {
+      const listOfErrors = ajv.errors.map(error => error.message);
+      throw new ExpressError(listOfErrors, 400);
+    }
+    const newUser = await User.register(req.body);
+    const token = createToken(newUser);
+    return res.status(201).json({ token });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+router.patch('/:username', async (req, res, next) => {
+  try {
+    if ('username' in req.body || 'is_admin' in req.body) {
+        throw new ExpressError(
+          'You are not allowed to change username or is_admin properties.',
+          400);
+    }
+
+    const valid = ajv.validate(userUpdateSchema, req.body);
+    if (!valid) {
+      const listOfErrors = ajv.errors.map(error => error.message);
+      throw new ExpressError(listOfErrors, 400);
+    }
+
+    const user = await User.update(req.params.username, req.body);
+    return res.json({ user });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+router.delete('/:username', async (req, res, next) => {
+  try {
+    await User.remove(req.params.username);
+    return res.json({ message: 'User deleted' });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+module.exports = router;
